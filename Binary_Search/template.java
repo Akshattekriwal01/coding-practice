2bed22d7472880be780fff99f8de9b59c9ef4351
akshattekriwal01 <akshattekriwal123@gmail.com>
2021-06-23 11:13:55 -0400

    update to binary search template

--- Binary_Search/template.java
+++ Binary_Search/template.java
@@ -18,25 +18,41 @@ class template{
         return -1 ;// element not found in the array
     }
 
-    // 2. Looking for the first element that satisfies certain properties. 
+    // 2a. Looking for the first element that satisfies certain properties. 
     // [F,F,T,T,T] <- returns first occurance of True 
-    public int template2(int[] nums ){
+    public int template2a(int[] nums ){
         int left = 0 ; int right = nums.length-1; //! add +1 if the answer can be outside [left,right ] when none of the values in the boudry gives true;
 
         while(left < right ){
-            int mid = left + (right - left)/2;
+            int mid = left + (right - left)/2;  //!round down. 
             if(property(mid,nums)){
                 right = mid;
             }else{
                 left = mid+1;
             }
         }
-        return left ; // returns n in case of [F,F,F,F,F]
+        // left == right so you can return anything it will work
+        return left ; // returns n in case of [F,F,F,F,F] / returns n when what what you are looking for is not present in the array 
     }
 
     public boolean property(int i , int[] nums){
             // example find first occurance of non- smaller number than 7;
              return nums[i] >= 7;       
+    }
+    // 2b. Looking for the first element that satisfies certain properties. 
+    // [F,F,T,T,T] <- returns first occurance of True 
+    public int template2b(int[] nums ){
+        int left = 0 ; int right = nums.length-1; //! add +1 if the answer can be outside [left,right ] when none of the values in the boudry gives true;
+
+        while(left < right ){
+            int mid = left + (right - left+1)/2;  //! ROUND UP WHEN doing mid-1 
+            if(property(mid,nums)){
+                left = mid;
+            }else{
+                right = mid-1;
+            }
+        }
+        return left ; // returns n in case of [F,F,F,F,F] / returns n when what what you are looking for is not present in the array 
     }
 
 }
