e9370c412fc7e40c1160f2d48205297b0ca4e2fd
Akshattekriwal01 <akshattekriwal123@gmail.com>
2021-07-04 17:20:09 -0400

    Time: 1 ms (32.06%), Space: 38.8 MB (29.05%) - LeetHub

--- validate-binary-search-tree/validate-binary-search-tree.java
+++ validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,41 +14,61 @@
  * }
  */
 class Solution {
-    public boolean isValidBST(TreeNode root) {
-        return morrisInOrder(root);
+    public void insertAllLeft(TreeNode node, LinkedList<TreeNode> stack){
+        while(node != null){
+            stack.addFirst(node);
+            node = node.left; 
+            
+        }
     }
-    public boolean morrisInOrder(TreeNode root){
+    
+    public boolean isValidBST(TreeNode root) {
+        LinkedList<TreeNode> stack = new LinkedList<TreeNode>();
+        insertAllLeft(root, stack);
         long prev = Long.MIN_VALUE;
-        TreeNode curr = root ;
-        while(curr != null){
-            TreeNode left = curr.left ; 
-            if(left == null){
-                System.out.println(curr.val);
-                 if(prev >= curr.val) return false;  
-                prev = curr.val; 
-                  curr = curr.right ;
-          
-            }else{ // create thread 
-                TreeNode rightMost = rightMost(left, curr);
-                if(rightMost.right == null){
-                    rightMost.right = curr; // Thread is created. 
-                        curr = curr.left ;
-                }else{ // delete thread ; 
-                    rightMost.right = null ;
-                   if(prev >= curr.val) return false;  
-                    prev = curr.val;
-                    curr = curr.right ;  
-                }
-                
-                
-            }
+        while(!stack.isEmpty()){
+            TreeNode curr = stack.removeFirst();
+            if(prev >= curr.val ) return false;
+            prev = curr.val;
+            insertAllLeft(curr.right,stack);
         }
         return true;
     }
-    public TreeNode rightMost(TreeNode left , TreeNode root){
-        while(left.right != null && left.right != root){
-            left = left.right;
-        }
-        return left; 
-    }
+//     public boolean isValidBST(TreeNode root) {
+//         return morrisInOrder(root);
+//     }
+//     public boolean morrisInOrder(TreeNode root){
+//         long prev = Long.MIN_VALUE;
+//         TreeNode curr = root ;
+//         while(curr != null){
+//             TreeNode left = curr.left ; 
+//             if(left == null){
+//                 System.out.println(curr.val);
+//                  if(prev >= curr.val) return false;  
+//                 prev = curr.val; 
+//                   curr = curr.right ;
+          
+//             }else{ // create thread 
+//                 TreeNode rightMost = rightMost(left, curr);
+//                 if(rightMost.right == null){
+//                     rightMost.right = curr; // Thread is created. 
+//                         curr = curr.left ;
+//                 }else{ // delete thread ; 
+//                     rightMost.right = null ;
+//                    if(prev >= curr.val) return false;  
+//                     prev = curr.val;
+//                     curr = curr.right ;  
+//                 }
+                
+                
+//             }
+//         }
+//         return true;
+//     }
+//     public TreeNode rightMost(TreeNode left , TreeNode root){
+//         while(left.right != null && left.right != root){
+//             left = left.right;
+//         }
+//         return left; 
+//     }
 }
