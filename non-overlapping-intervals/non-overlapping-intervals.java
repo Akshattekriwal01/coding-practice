956768bf77e3158befc051b43de188808ab2bff6
Akshattekriwal01 <akshattekriwal123@gmail.com>
2021-08-02 17:48:03 -0400

    Time: 47 ms (9.12%), Space: 96.7 MB (5.45%) - LeetHub

--- non-overlapping-intervals/non-overlapping-intervals.java
+++ non-overlapping-intervals/non-overlapping-intervals.java
@@ -9,7 +9,13 @@ intervals non-overlapping]
 class Solution{
     public int eraseOverlapIntervals(int[][] intervals){
         // sort by finish time. 
-        Arrays.sort(intervals, (a,b) -> a[1]-b[1] ) ; 
+           Comparator<int[]> myCompare = new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1]-o2[1];
+            }
+        };
+        Arrays.sort(intervals, myCompare ) ; 
         // total - nonOverlapping = intervals to be removed ; 
         int count = 0 ; // stores the min number of overlaping intervals. 
         /*
@@ -32,38 +38,7 @@ class Solution{
             }
         }
        return count ;
-        
-        // Alternatively {all intervals} - {max compatible intervals} = minimum deleted intervals
-        
-        /*
-        count = 0 ;
-        int prevFinish = intervals[0][1]; 
-        for(int i = 1 ; i < intervals.length ;i++){
-            int currStart = intervals[i][0]; 
-            int currFinish = intervals[i][1];
-            if( currStart < prevFinish){
-                
-                prevFinish = Math.min(prevFinish, currFinish); // remove the one with longer finsih time when 2 intervals overlap. 
-            }else{
-                count ++ ;
-                prevFinish = currFinish ; 
-            }
-        }
-       return intervals.length - count - 1; 
-       */
-        /*
-        int end = intervals[0].end;
-        int count = 1;        
-
-        for (int i = 1; i < intervals.length; i++) {
-            if (intervals[i].start >= end) {
-                end = intervals[i].end;
-                count++;
-            }
-        }
-        return intervals.length - count;
-    }
-        */
+      
     }
 }
 
