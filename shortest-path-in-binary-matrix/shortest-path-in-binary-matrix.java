9228147fab7d72aedb765f9f2e401da998a4cd58
Akshattekriwal01 <akshattekriwal123@gmail.com>
2021-08-08 16:10:15 -0400

    Time: 19 ms (35.43%), Space: 40.2 MB (58.81%) - LeetHub

---
+++ shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -0,0 +1,57 @@
+/*
+Take Away.
+a. in 8 directional grid. level != shortest path. 
+b. when finding these paths types of question . when we add the first element to the q. check is it is a valid element
+c. Do the "Work" part of a cyclic bfs outside the "add neighbour loop"; 
+c. know what does to exit while loop in bfs. 
+d. 2d bfs is easier to debug and understand .
+*/
+
+class Solution {
+    
+    
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        Queue<int[]> q = new  LinkedList<int[]>() ; 
+        int rows = grid.length; 
+        int columns = grid[0].length ; 
+        int[] arr = new int[3]; // i,j,count
+        
+        arr[0] = 0 ; 
+        arr[1] = 0 ; 
+        arr[2] = 1 ; 
+        if(grid[0][0] == 0 )
+            q.add(arr); 
+        
+        while(!q.isEmpty()){
+            int[] rem = q.poll(); 
+            
+               if( rem[0] == (rows-1) && rem[1] == (columns-1) ) return rem[2] ; 
+            
+            int dir[][] = {{-1,-1}, {-1,0}, {-1,1},{0,1}, {1,1}, {1,0}, {1,-1}, {0,-1}};
+                for(int d = 0 ; d < 8 ; d++){
+                    int r = rem[0] + dir[d][0] ;
+                    int c = rem[1] + dir[d][1] ;
+                    
+                 
+                    
+                    if( r < rows && c < columns && r >= 0 && c >= 0 && grid[r][c] == 0){
+                        int [] arr1 = new int [3]; 
+                        arr1[0] = r ;
+                        arr1[1] = c ; 
+                        arr1[2] = rem[2] + 1 ;
+                        q.add(arr1) ;
+                        grid[r][c] = 1 ; 
+                        
+                        
+                    }
+                }
+        }
+        /*[1,0,0],
+          [1,1,0],
+          [1,1,0]
+        */
+        return -1; // when we reach here means that during the bfs 
+                        // a. the element was present but could't be reached . 
+                        // b. the element was not present .
+    }
+}
