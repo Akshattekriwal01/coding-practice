e83d028ecb38bbefcd2f3b6e6999453abc038676
Akshattekriwal01 <akshattekriwal123@gmail.com>
2021-09-04 15:27:28 -0400

    Time: 1 ms (75.37%), Space: 39.2 MB (64.96%) - LeetHub

--- generate-parentheses/generate-parentheses.java
+++ generate-parentheses/generate-parentheses.java
@@ -1,101 +1,41 @@
-/*
-brute force generate all possible paranthesis of length 3 and checks which one is valid. 
-number of b at any point cannot exceed number of a at that point. 
-number of a cannot exceed n at any point ; 
-
-aaabbb
-ababab
-aababb
-aabbab
-abaabb
-
-
-abb x
-aaaa x number a a exceeded 3
-abaaa x
-aabaa x
-aaaba x
-ababb x
-aabbb x
-ababaa x 
-aabbaa x 
-abaaba  x
-aababa x
-aaabba x
-ababab valid
-aabbab valid
-abaabb valid
-aababb valid
-aaabbb valid
-=========================================
-N = 2
-A=3
-B=3
-aaabbb
-
-
-*/
-
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> ans = new ArrayList<String>(); 
-        helper(ans , "(", n , 1 , 0);
-        return ans;
+        ArrayList<String> al = new ArrayList<>() ;
+        helper(al, "(", 1 , 0, n);
+        return al;
     }
-    public void helper(List<String> ans, String sofar, int n , int countA , int countB){
-        if(sofar.length() == n*2){
-            if (countA == n && countB == n){
-                ans.add(sofar);
-            }
-            return ; 
+    
+    void helper(ArrayList<String> al , String sofar , int countA, int countB, int n ){
+        
+        if(countB > countA){
+            return ;
+        }else if(sofar.length() == 2*n ){
+            al.add(sofar);
+            return ;
         }else{
-            if(countA < n){
-            String s1 = sofar+"(";
-            helper(ans,s1,n,countA+1,countB);
-            }
-          
-            if(countB < countA){
-            String s2 = sofar+")";
-            helper(ans,s2,n,countA,countB+1);
-            }
-   
+            
+            if(countA< n)
+             helper(al, sofar+"(", countA+1, countB,n);
+            
+            
+            helper(al, sofar+")", countA , countB+1,n) ;
+            
+            return ;
+            
         }
-        return ;
+        
     }
 }
 
 
+/*
+given n a's and n b's i have to pemute them such that at ith poitions number of a's >= b's ;
+
+aaabbb
+aababb
+aabbab
+abaabb
+ababab
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
