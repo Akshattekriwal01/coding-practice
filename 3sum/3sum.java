ccb224343902784483e6a1485bb791a5a1deaf05
Akshattekriwal01 <akshattekriwal123@gmail.com>
2021-09-15 14:24:10 -0400

    Time: 16 ms (97.83%), Space: 42.6 MB (89.18%) - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -5,30 +5,40 @@
    // so time complexity is O(n^2);
 */
 class Solution {
-   
+   /*
+   sort to avoid duplicates for history.
+   2 sum for sorted is using high and low.
+   in case of high and low 
+   */
     
     
  public List<List<Integer>> threeSum(int[] num) {
-    Arrays.sort(num);
-    List<List<Integer>> res = new LinkedList<>(); 
-    for (int i = 0; i < num.length-2; i++) {
-        if (i == 0 || (i > 0 && num[i] != num[i-1])) {
-            int lo = i+1, hi = num.length-1, sum = 0 - num[i];
-            while (lo < hi) {
-                if (num[lo] + num[hi] == sum) {
-                    res.add(Arrays.asList(num[i], num[lo], num[hi]));
-                    while (lo < hi && num[lo] == num[lo+1]) lo++;
-                    while (lo < hi && num[hi] == num[hi-1]) hi--;
-                    lo++; hi--;
-                } else if (num[lo] + num[hi] < sum) lo++;
-                else hi--;
-           }
+// sorting is to avoid duplicated, we don't look at the past. 
+     // since the number is sorted, we can use 2 pointers to find if the sum exist
+    
+     Arrays.sort(num); // cannot use lambda function for this. 
+     List<List<Integer>> res = new ArrayList<List<Integer>>() ;
+    for(int i = 0 ; i < num.length - 1  ; i++){
+        if(i > 0 && num[i] == num[i-1]) continue ;
+        int target = -1 * num[i] ; 
+        int l = i+1 ; 
+        int r = num.length - 1;
+          List<Integer> ans = new ArrayList<Integer>() ;
+        while(l < r){
+            int sum = num[l]+num[r];
+        if(sum > target ) r-- ;
+        else if( sum < target) l++ ;
+        else { // sum == target 
+            res.add(Arrays.asList(num[l],num[r],num[i]));
+           while( l < r && num[l] == num[l+1]) l++ ;
+           while( l < r && num[r] == num[r-1]) r-- ;
+            l++ ;
+            r-- ;
         }
+     }
+    
     }
-    return res;
-}   
-    
-    
-    
-    
+     return res;
+ }   
+       
 }
