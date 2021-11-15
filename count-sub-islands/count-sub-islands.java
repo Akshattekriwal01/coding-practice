50305972d002905d196510b24e5cea263a05370d
Akshattekriwal01 <akshattekriwal123@gmail.com>
2021-11-14 20:43:20 -0500

    Time: 33 ms (34.26%), Space: 334.7 MB (5.07%) - LeetHub

--- count-sub-islands/count-sub-islands.java
+++ count-sub-islands/count-sub-islands.java
@@ -1,38 +1,35 @@
 class Solution {
     public int countSubIslands(int[][] grid1, int[][] grid2) {
-        int n = grid1.length ; 
-        int m = grid1[0].length ; 
-         int count = 0 ;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(grid2[i][j] == 1){
-                    if(dfs(i,j,grid1,grid2)){
-                        count++ ;
-                    } 
-                }
-            } 
+        int m  = grid1.length ; 
+        int n = grid1[0].length;
+        int count = 0 ;
+        for(int i = 0 ; i < m; i++ ){
+            for (int j = 0 ; j < n ; j++){
+               if(grid2[i][j] == 1)
+               count =  dfs(i,j,grid1,grid2 ) ? count + 1 : count ;
+            }
         }
         return count ;
     }
     
-    public boolean dfs(int i , int j, int[][] grid1, int[][] grid2){
-        int n = grid1.length ; 
-        int m = grid1[0].length ; 
-        
-        boolean flag =  grid1[i][j] == 1 ;
+    public boolean dfs(int i , int j ,int[][] grid1, int[][] grid2 ){
+        //visited 
         grid2[i][j] = -1 ; 
-        int[][] dir = {{0,1},{1,0},{-1,0},{0,-1}};
-        boolean ans  =  true; 
+        int m  = grid1.length ; 
+        int n = grid1[0].length;
+        boolean land = true ; 
+        if(grid1[i][j] == 0)
+            land = false ; 
+        
+        int[][] dir = {{-1,0},{0,1},{1,0},{0,-1}};
         for(int d = 0 ; d < 4 ; d++){
-            int r = i + dir[d][0];
-            int c = j + dir[d][1];
+            int row = i + dir[d][0]; 
+            int col = j + dir[d][1];
             
-            if(r >= 0 && c >=0 && r < n && c < m && grid2[r][c] == 1 ){
-              ans = ans & dfs(r,c,grid1,grid2);
-            }
+         if(row >= 0 && col >=0 && row < m && col < n && grid2[row][col] == 1 ){
+             land = land & dfs(row,col, grid1, grid2);
+         }  
         }
-        
-        return flag && ans ;
-        
+        return land ;  
     }
 }
